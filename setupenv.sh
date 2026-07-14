3831dbadc6fac685fdec1703fd068c58e71f0c8f
ranjitkav1 <53061868+ranjitkav1@users.noreply.github.com>
2026-07-14 14:24:01 -0500

    Set permissions for pytubefix package directory
    Grant full permissions to the pytubefix package directory.

--- setupenv.sh
+++ setupenv.sh
@@ -5,3 +5,4 @@ sudo apt install python3 python3-pip dos2unix
 sudo pip3 install argparse 
 sudo pip3 install -U pytubefix
 sudo apt update
+sudo chmod -R 777 /usr/local/lib/python3.12/dist-packages/pytubefix/

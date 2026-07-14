9c3a96ad9ceab7860bda61778499961b8a284ebb
ranjitkav1 <53061868+ranjitkav1@users.noreply.github.com>
2026-07-14 14:49:10 -0500

    Update pytubefix installation command

--- setupenv.sh
+++ setupenv.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install python3 python3-pip dos2unix
 sudo pip3 install argparse 
 sudo pip3 install -U pytubefix
-sudo rm -f /usr/local/lib/python3.12/dist-packages/pytubefix/__cache__/tokens.json
+sudo pip install --upgrade pytubefix
 sudo apt update
 sudo chmod -R 777 /usr/local/lib/python3.12/dist-packages/pytubefix/

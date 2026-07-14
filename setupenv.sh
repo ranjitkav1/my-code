9066749448fbd8b9e7316d073fb49bda8aba0354
ranjitkav1 <53061868+ranjitkav1@users.noreply.github.com>
2026-07-14 12:53:22 -0500

    Separate pip install commands for clarity

--- setupenv.sh
+++ setupenv.sh
@@ -2,5 +2,6 @@
 
 sudo apt update
 sudo apt install python3 python3-pip dos2unix
-sudo pip3 install argparse pytubefix=8.11.0
+sudo pip3 install argparse 
+sudo pip3 install pytubefix=8.11.0
 sudo apt update

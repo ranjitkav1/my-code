b79cdd688cd0762ea4dec6428ce8ec03ae171a8d
ranjitkav1 <53061868+ranjitkav1@users.noreply.github.com>
2026-07-14 14:28:09 -0500

    Add installation of Git from GitHub repository

--- setupenv.sh
+++ setupenv.sh
@@ -4,5 +4,6 @@ sudo apt update
 sudo apt install python3 python3-pip dos2unix
 sudo pip3 install argparse 
 sudo pip3 install -U pytubefix
+sudo pip3 install -U git+https://github.com
 sudo apt update
 sudo chmod -R 777 /usr/local/lib/python3.12/dist-packages/pytubefix/
